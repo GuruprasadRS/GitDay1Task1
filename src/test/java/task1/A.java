eb5dad029172bdfb16382d3d63529a12fc617f11
GuruprasadRS <guruprasad.off.use@gmail.com>
2020-05-01 13:13:30 +0530

    1st Project

--- src/test/java/task1/A.java
+++ src/test/java/task1/A.java
@@ -1,5 +1,14 @@
 package task1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class A {
- 
+  public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Maven-Workspace\\GitDay1Task1\\driver\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	driver.quit();
+}
 }
