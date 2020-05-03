13287a491b5aaa9603c842c1d2437dff53537b95
GuruprasadRS <guruprasad.off.use@gmail.com>
2020-05-03 10:11:52 +0530

    Guru1 Updates Codes

--- src/test/java/task1/A.java
+++ src/test/java/task1/A.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class A {
   public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Maven-Workspace\\GitDay1Task1\\driver\\chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Desktop\\Clonned Project 2\\GitDay1Task1\\driver\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
+	String title = driver.getTitle();
+	System.out.println(title);
 	driver.quit();
 }
 }
