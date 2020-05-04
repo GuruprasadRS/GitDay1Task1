ea7e81eb9ae9619146d70776611a4ba03acf29e4
GuruprasadRS <guruprasad.off.use@gmail.com>
2020-05-04 15:09:44 +0530

    updated code now 04.05.2020

--- src/test/java/task1/A.java
+++ src/test/java/task1/A.java
@@ -1,10 +1,27 @@
+
 package task1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class A {
-  public static void main(String[] args) {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Desktop\\Clonned Project 2\\GitDay1Task1\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		driver.quit();
+		driver.close();
+		String pageSource = driver.getPageSource();
+		System.out.println(pageSource);
+		
+	}
+}
+ /* public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Guru\\Desktop\\Clonned Project 2\\GitDay1Task1\\driver\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.facebook.com/");
@@ -14,3 +31,4 @@ public class A {
 	driver.quit();
 }
 }
+*/
